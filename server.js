7c921a3b67e0bf70e7eb8b9f777d0f66dd507ec9
dashort <dashort@gmail.com>
2025-05-04 14:45:31 -0500

    Fix: update server to use port 8080 for Railway

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
